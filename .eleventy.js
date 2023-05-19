f4e8836af02b1d913fc89ca57cad5a14b09a4752
Glauco C <glau@users.noreply.github.com>
2023-05-19 16:15:02 -0300

    Setup basice I18n eleventy configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    defaultLanguage: "en", // Required, this site uses "en"
+  });
+
   return {
     dir: {
       input: "src",

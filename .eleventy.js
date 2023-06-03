a1424d6b04af1b9acbfb8110a4d6b1e743ac3f85
Glauco C <glau@users.noreply.github.com>
2023-06-03 19:52:40 -0300

    Adopt Lene Saile approach to string internationalization

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const { EleventyI18nPlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
-    defaultLanguage: "pt",
+    defaultLanguage: "en",
   });
   
   eleventyConfig.addPassthroughCopy("bundle.css");
---
+++ src/_data/languages.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pt: {
+    dir: '',
+    availableText: 'Esta página também está disponível em:',
+    toHomePage: 'Voltar para a página inicial',
+  },
+  en: {
+    dir: '',
+    availableText: 'This page is also available in:',
+    toHomePage: 'Back to the home page',
+  }
+};

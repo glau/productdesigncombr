511d5ea86b3651ff817aa089d1e114f7809f15fe
Glauco C <glau@users.noreply.github.com>
2023-06-06 14:03:39 -0300

    Ajustes no rodapé

--- src/_data/languages.js
+++ src/_data/languages.js
@@ -3,10 +3,16 @@ module.exports = {
     dir: '',
     availableText: 'Esta página também está disponível em:',
     toHomePage: 'Voltar para a página inicial',
+    siteBuiltWith: 'Eu construí este site com o',
+    siteSchedule: 'e eu cuido dele como um',
+    digitalGarden: 'Jardim Digital'
   },
   en: {
     dir: '',
     availableText: 'This page is also available in:',
     toHomePage: 'Back to the home page',
+    siteBuiltWith: 'I built this website with',
+    siteSchedule: 'and I tend to it as a',
+    digitalGarden: 'Digital Garden'
   }
 };
